5c27e3687998ebca5df53b2f0ca6b6d418aef7c5
basarat <basaratali@gmail.com>
2014-06-21 22:42:19 +1000

    package activated!

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,15 @@
-console.log('Package loaded');
+interface PackageState{
+
+}
+
+export function activate(state:PackageState){
+  // console.log('typescript package activated!');
+}
+
+export function serialize(): PackageState{
+  return {};
+}
+
+export function deserialize(){
+  /* do any tear down here */
+}
