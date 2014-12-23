6d7c193d314685cce42a64b9757d1e8c2baa15a1
basarat <basaratali@gmail.com>
2014-12-23 16:10:11 +1100

    added an APM package dependency on linter

---
+++ node_modules/atom-package-dependencies/node_modules/witwip/test/local/carmen/index.js
@@ -0,0 +1,10 @@
+/**package
+ * {
+ *   "name": "carmen",
+ *   "version": "0.0.1",
+ *   "description": "A module that uses a package comment.",
+ *   "private": true
+ * }
+ **/
+
+module.exports = 'I am Carmen Sandiego';
