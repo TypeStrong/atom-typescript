8448317506df08aa0569b80e45012c72c8938935
Basarat Syed <bas@picnicsoftware.com>
2015-01-19 16:41:36 +1100

    figured out how to setup linter

--- lib/main/errors.ts
+++ lib/main/errors.ts
@@ -1,11 +1,2 @@
 ///ts:ref=globals
 /// <reference path="../globals.ts"/> ///ts:ref:generated
-
-var linterPath = atom.packages.getLoadedPackage("linter").path;
-var Linter = require(linterPath + "/lib/linter");
-
-//class Foo extends Linter {
-//    constructor() {
-//        console.log('called');
-//    }
-//}
