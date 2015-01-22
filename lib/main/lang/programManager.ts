b9cf612735e0e0ffa656f459cd1bc0730b9d921a
Basarat Syed <bas@picnicsoftware.com>
2015-01-22 18:25:14 +1100

    added initial emit. Fails because apparently I need to walk all `referneces` myself as language service isn't doing it

--- lib/main/lang/programManager.ts
+++ lib/main/lang/programManager.ts
@@ -1,20 +1,51 @@
 ///ts:ref=globals
 /// <reference path="../../globals.ts"/> ///ts:ref:generated
 
+import fs = require('fs');
+import ts = require('typescript');
+
 ///ts:import=tsconfig
 import tsconfig = require('../tsconfig/index'); ///ts:import:generated
 import LanguageServiceHost = require('./languageServiceHost');
 
-import ts = require('typescript');
-
-
 export class Program {
-    public languageServiceHost: LanguageServiceHost; 
+    public languageServiceHost: LanguageServiceHost;
     public languageService: ts.LanguageService;
 
     constructor(public projectFile: tsconfig.TypeScriptProjectFileDetails) {
         this.languageServiceHost = new LanguageServiceHost(projectFile);
         this.languageService = ts.createLanguageService(this.languageServiceHost, ts.createDocumentRegistry());
+
+        // Since we only create a program per project once. Emit the first time 
+        this.projectFile.project.files.forEach((filename) => this.emitFile(filename));
+    }
+
+    emitFile = (filename: string) => {
+        var services = this.languageService;
+        var output = services.getEmitOutput(filename);
+
+        if (output.emitOutputStatus === ts.EmitReturnStatus.Succeeded) {
+            console.log('SUCCESS ' + filename);
+        }
+        else {
+            console.log('FAILURE ' + filename + ' emit');
+            var allDiagnostics = services.getCompilerOptionsDiagnostics()
+                .concat(services.getSyntacticDiagnostics(filename))
+                .concat(services.getSemanticDiagnostics(filename));
+
+            console.log(allDiagnostics);
+            allDiagnostics.forEach(diagnostic => {
+                if (!diagnostic.file) return; // TODO: happens only for 'lib.d.ts' for now
+
+                console.log('diagnostic', diagnostic);
+                var lineChar = diagnostic.file.getLineAndCharacterFromPosition(diagnostic.start);
+                console.log(diagnostic.file && diagnostic.file.filename, lineChar.line, lineChar.character, diagnostic.messageText);
+            });
+        }
+
+        output.outputFiles.forEach(o => {
+            fs.writeFileSync(o.name, o.text, "utf8");
+        });
     }
 }
 
