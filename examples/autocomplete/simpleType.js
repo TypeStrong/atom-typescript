c85b69531c9b5ad09faa048286d68f9a208ec29a
Basarat Syed <basaratali@gmail.com>
2015-02-16 18:08:06 +1100

    test for completions in object literal and function returns refs #82

---
+++ examples/autocomplete/simpleType.js
@@ -0,0 +1,4 @@
+function w(x) {
+    return '';
+}
+w({});
---
+++ examples/autocomplete/simpleType.ts
@@ -0,0 +1,5 @@
+function w(x: { test: string }) { return ''; }
+// Press Ctrl + space below
+w({ /*before here*/ });
+// Press Ctrl + space below
+w({test:''})./*before here*/
