7cd66e8629bf8b3d73214b245170160838840823
Basarat Syed <basaratali@gmail.com>
2015-02-18 19:00:58 +1100

    added a production target

---
+++ examples/multipleCompileTargets/src/server/client/app.js
@@ -0,0 +1,2 @@
+!function(o){function e(r){if(n[r])return n[r].exports;var t=n[r]={exports:{},id:r,loaded:!1};return o[r].call(t.exports,t,t.exports,e),t.loaded=!0,t.exports}var n={};return e.m=o,e.c=n,e.p="",e(0)}([function(o,e,n){o.exports=n(2)},function(o,e){var n=function(){function o(){console.log("awesome1"),console.log("awesome2"),console.log("awesome3"),console.error("error")}return o}();e.Awesome=n},function(o,e,n){var r=n(1);e.client=new r.Awesome}]);
+//# sourceMappingURL=app.js.map
--- examples/multipleCompileTargets/src/server/client/app/awesome.ts
+++ examples/multipleCompileTargets/src/server/client/app/awesome.ts
@@ -2,5 +2,7 @@ export class Awesome {
     constructor() {
         console.log('awesome1');
         console.log('awesome2');
+        console.log('awesome3');
+        console.error('error');
     }
 }
