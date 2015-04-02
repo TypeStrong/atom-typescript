f58de230dd6474b24586cccd5b463831baef4c3a
Basarat Syed <basaratali@gmail.com>
2015-04-02 19:23:27 +1100

    chore(:memo:) minor english fix closes 2228

--- lib/main/atom/views/mainPanelView.ts
+++ lib/main/atom/views/mainPanelView.ts
@@ -113,7 +113,7 @@ export class MainPanelView extends view.View<any> {
         this.buildBody.html('<span class="text-success"> No Build. Press (ctrl+shift+b / cmd+shift+b ) to start a build for an active TypeScript file\'s project. </span>')
 
         this.referencesPanelBtn.html(`${panelHeaders.references} ( <span class="text-success">No Search</span> )`)
-        this.referencesBody.html('<span class="text-success"> You haven\'t search for TypeScript references yet. </span>')
+        this.referencesBody.html('<span class="text-success"> You haven\'t searched for TypeScript references yet. </span>')
     }
 
     errorPanelSelected(forceExpand = true) {
