bcdfdded9aa361418c9c4b56b8ddda3845fe1d77
Basarat Ali Syed <basaratali@gmail.com>
2015-04-29 21:05:06 +1000

    fix(typescript) fix(performance) manually loading all the service files one by one was *much* slower refs #314

--- dist/typescript/makeTypeScriptGlobal.js
+++ dist/typescript/makeTypeScriptGlobal.js
@@ -1,48 +1,5 @@
-var servicesFiles = [
-    "../compiler/core.ts",
-    "../compiler/sys.ts",
-    "../compiler/types.ts",
-    "../compiler/scanner.ts",
-    "../compiler/parser.ts",
-    "../compiler/utilities.ts",
-    "../compiler/binder.ts",
-    "../compiler/checker.ts",
-    "../compiler/emitter.ts",
-    "../compiler/program.ts",
-    "../compiler/declarationEmitter.ts",
-    "../compiler/diagnosticInformationMap.generated.ts",
-    "../compiler/commandLineParser.ts",
-    "breakpoints.ts",
-    "navigateTo.ts",
-    "navigationBar.ts",
-    "outliningElementsCollector.ts",
-    "patternMatcher.ts",
-    "services.ts",
-    "shims.ts",
-    "signatureHelp.ts",
-    "utilities.ts",
-    "formatting/formatting.ts",
-    "formatting/formattingContext.ts",
-    "formatting/formattingRequestKind.ts",
-    "formatting/formattingScanner.ts",
-    "formatting/references.ts",
-    "formatting/rule.ts",
-    "formatting/ruleAction.ts",
-    "formatting/ruleDescriptor.ts",
-    "formatting/ruleFlag.ts",
-    "formatting/ruleOperation.ts",
-    "formatting/ruleOperationContext.ts",
-    "formatting/rules.ts",
-    "formatting/rulesMap.ts",
-    "formatting/rulesProvider.ts",
-    "formatting/smartIndenter.ts",
-    "formatting/tokenRange.ts"
-];
-var files = servicesFiles.map(function (f) { return ("./services/" + f.replace('.ts', '.js')); });
 var vm = require('vm');
 var fs = require('fs');
-var os = require('os');
-var path = require('path');
 global.stack = function () {
     console.error((new Error()).stack);
 };
@@ -57,12 +14,7 @@ function makeTsGlobal(typescriptServices) {
         vm.runInContext(fs.readFileSync(typescriptServices).toString(), sandbox);
     }
     else {
-        var fileContents = [];
-        files.forEach(function (f) { return fileContents.push(fs.readFileSync(path.resolve(__dirname, f)).toString()); });
-        var fileContent = fileContents.join(os.EOL);
-        files.forEach(function (f) {
-            vm.runInContext(fileContent, sandbox);
-        });
+        vm.runInContext(fs.readFileSync(require.resolve('typescript')).toString(), sandbox);
     }
     global.ts = sandbox.ts;
 }
--- lib/typescript/makeTypeScriptGlobal.ts
+++ lib/typescript/makeTypeScriptGlobal.ts
@@ -1,51 +1,3 @@
-// Put the whole of `ts` namespace into the global `ts` variable
-// IMPORTANT!!!!!!!!!!!! `diagnosticInformationMap` needs to be before `commandLineParser`
-// Also a few files were missing that I added manually below
-// TODO: replace with a glob that only takes into account the few file order we care about
-var servicesFiles = [
-    "../compiler/core.ts",
-    "../compiler/sys.ts",
-    "../compiler/types.ts",
-    "../compiler/scanner.ts",
-    "../compiler/parser.ts",
-    "../compiler/utilities.ts",
-    "../compiler/binder.ts",
-    "../compiler/checker.ts",
-    "../compiler/emitter.ts",
-    "../compiler/program.ts",
-    "../compiler/declarationEmitter.ts",
-    "../compiler/diagnosticInformationMap.generated.ts",
-    "../compiler/commandLineParser.ts",
-    "breakpoints.ts",
-    "navigateTo.ts",
-    "navigationBar.ts",
-    "outliningElementsCollector.ts",
-    "patternMatcher.ts",
-    "services.ts",
-    "shims.ts",
-    "signatureHelp.ts",
-    "utilities.ts",
-    "formatting/formatting.ts",
-    "formatting/formattingContext.ts",
-    "formatting/formattingRequestKind.ts",
-    "formatting/formattingScanner.ts",
-    "formatting/references.ts",
-    "formatting/rule.ts",
-    "formatting/ruleAction.ts",
-    "formatting/ruleDescriptor.ts",
-    "formatting/ruleFlag.ts",
-    "formatting/ruleOperation.ts",
-    "formatting/ruleOperationContext.ts",
-    "formatting/rules.ts",
-    "formatting/rulesMap.ts",
-    "formatting/rulesProvider.ts",
-    "formatting/smartIndenter.ts",
-    "formatting/tokenRange.ts"
-];
-
-
-var files = servicesFiles.map(f=> `./services/${f.replace('.ts', '.js') }`);
-
 ////////////////////////////////// MAGIC
 import vm = require('vm');
 import fs = require('fs');
@@ -70,12 +22,7 @@ export function makeTsGlobal(typescriptServices?: string) {
         vm.runInContext(fs.readFileSync(typescriptServices).toString(), sandbox);
     }
     else {
-        let fileContents = [];
-        files.forEach(f=> fileContents.push(fs.readFileSync(path.resolve(__dirname, f)).toString()));
-        let fileContent = fileContents.join(os.EOL);
-        files.forEach(f=> {
-            vm.runInContext(fileContent, sandbox);
-        });
+        vm.runInContext(fs.readFileSync(require.resolve('typescript')).toString(), sandbox);
     }
 
     // Finally export ts to the local global namespace
