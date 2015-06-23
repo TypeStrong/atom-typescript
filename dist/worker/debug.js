afb5d86fc706354930ef253f050dd8556fb045e1
Basarat Syed <basaratali@gmail.com>
2015-06-23 11:31:05 +1000

    feat(typescript) A simpler internal API + nightly build workflow. Done using `ntypescript`

--- dist/worker/debug.js
+++ dist/worker/debug.js
@@ -1,3 +1,2 @@
 exports.debugAll = false;
 exports.debugSync = false || exports.debugAll;
-exports.debugLanguageService = false || exports.debugAll;
--- lib/worker/debug.ts
+++ lib/worker/debug.ts
@@ -3,6 +3,3 @@ export var debugAll = false;
 
 /** Set this to true to run the child code in the UI thread and just debug using the dev tools */
 export var debugSync = false || debugAll;
-
-/** Set this to true if you want to use to a file by file version of language service instead of the one in package.json */
-export var debugLanguageService = false || debugAll;
