4773c096768dd08b4da18af4d0825454eb0cb084
Victor M <gobnetzo@gmail.com>
2015-06-26 13:01:39 +0500

    Clean up

--- dist/main/atom/fileStatus.js
+++
@@ -1,7 +0,0 @@
-var mainPanelView_1 = require("./views/mainPanelView");
-exports.fileStatuses = [];
-function updateFileStatus(filePath, status) {
-    exports.fileStatuses[filePath] = status;
-    mainPanelView_1.panelView.updateFileStatus(status);
-}
-exports.updateFileStatus = updateFileStatus;
