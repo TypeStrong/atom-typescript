1778f7aae34c2010cfc543e15575df0a8d6c1749
Basarat Syed <basaratali@gmail.com>
2015-10-08 16:29:52 +1100

    minor : rebuild with self :rose:

--- lib/globals.ts
+++ lib/globals.ts
@@ -4,7 +4,6 @@
 /// <reference path="./typings/bluebird.d.ts"/>
 /// <reference path="./typings/codemirror.d.ts"/>
 /// <reference path="./typings/brackets.d.ts"/>
-/// <reference path="./typings/minimatch.d.ts"/>
 /// <reference path="./typings/mustache.d.ts"/>
 
 /// <reference path="../views/views.d.ts"/>
