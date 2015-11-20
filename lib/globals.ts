281ee07cbed808fc6894f03274e98f6817f3975f
Basarat Syed <basaratali@gmail.com>
2015-11-20 16:59:59 +1100

    fix(build) will it build?

--- lib/globals.ts
+++ lib/globals.ts
@@ -1,4 +1,5 @@
 /// <reference path="../node_modules/ntypescript/bin/ntypescript.d.ts"/>
+/// <reference path="../node_modules/tsconfig/dist/tsconfig.d.ts"/>
 /// <reference path="./typings/tsd.d.ts"/>
 
 // From brackets plugin
