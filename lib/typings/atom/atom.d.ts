55c47cd0192659d18d056eff57a1a5325ea6f57f
Basarat Syed <basaratali@gmail.com>
2016-01-25 16:52:52 +1100

    chore(compile) move more stuff to typings :rose:

--- lib/typings/atom/atom.d.ts
+++ lib/typings/atom/atom.d.ts
@@ -7,7 +7,6 @@
 /// <reference path="../jquery/jquery.d.ts" />
 /// <reference path="../space-pen/space-pen.d.ts" />
 /// <reference path="../emissary/emissary.d.ts" />
-/// <reference path="../pathwatcher/pathwatcher.d.ts" />
 /// <reference path="../text-buffer/text-buffer.d.ts" />
 /// <reference path="../status-bar/status-bar.d.ts" />
 
