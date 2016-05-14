cbc2382b5c1c7c8aa832a6664ed52b28d266a617
Gregor Woiwode <gregor.woiwode@gmail.com>
2016-05-15 00:12:57 +0200

    Issue 818/angular component html highlighing (#948)
    * Adds custom grammar enabling html syntax highlighting
    Applies for @Component decorator provided by Angular 2
    #818
    
    * Applies grammar for html syntax highlighting to ts.cson and tsx.cson
    This is done by running `npm run build`
    #818
    
    * Allows backticks inside html-templates without breaking syntax-highlighting
    
    * Updates TypeScript grammar based on grammar.js
    Done by running npm run build

--- scripts/grammar.js
+++ scripts/grammar.js
@@ -94,6 +94,18 @@ var atomPatterns = [
         name: 'es6import.path.string.quoted'
       }
     }
+  },
+  {
+    comment: 'Match Angular 2 Component html templates',
+    begin: '(?:(?<=^|\\stemplate:)|(?<=/\\*\\* @html \\*/))\\s*(`)',
+    beginCaptures: {
+      '1': { name: 'name: string.template.ts' }
+    },
+    end: '(?<!\\\\)`',
+    endCaptures: {
+      '0': { name: 'name: string.template.ts' }
+    },
+    patterns: [ { include: 'text.html.basic' } ]
   }
 ]
 
