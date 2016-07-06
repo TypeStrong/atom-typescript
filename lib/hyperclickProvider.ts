3211be75cc48a3ba4d818ce92d3b27ebc5ef82a2
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-07-06 22:49:11 +0700

    Custom word matching to match token and string
    Ref #448

--- lib/hyperclickProvider.ts
+++ lib/hyperclickProvider.ts
@@ -6,6 +6,8 @@ const TS_GRAMMARS = Set<string>(["source.ts", "source.tsx"]);
 
 export let providerName = "typescript-hyperclick-provider";
 
+export let wordRegExp = /([A-Za-z0-9])+|['"`](\\.|[^'"`\\\\])*['"`]/g;
+
 export function getSuggestionForWord(textEditor: AtomCore.IEditor, text: string, range: TextBuffer.IRange) {
     return {
         range: range,
