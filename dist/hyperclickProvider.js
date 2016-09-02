720f834bd5d8362966be39913bb2cd527e1b0918
Asa Ayers <Asa@AsaAyers.com>
2016-09-02 08:44:20 -0700

    Prevent returning suggestions for files that aren't TypeScript

--- dist/hyperclickProvider.js
+++ dist/hyperclickProvider.js
@@ -6,12 +6,12 @@ var TS_GRAMMARS = immutable_1.Set(["source.ts", "source.tsx"]);
 exports.providerName = "typescript-hyperclick-provider";
 exports.wordRegExp = /([A-Za-z0-9_])+|['"`](\\.|[^'"`\\\\])*['"`]/g;
 function getSuggestionForWord(textEditor, text, range) {
+    if (!TS_GRAMMARS.has(textEditor.getGrammar().scopeName)) {
+        return null;
+    }
     return {
         range: range,
         callback: function () {
-            if (!TS_GRAMMARS.has(textEditor.getGrammar().scopeName)) {
-                return null;
-            }
             var filePathPosition = {
                 filePath: textEditor.getPath(),
                 position: atomUtils.getEditorPositionForBufferPosition(textEditor, range.start)
