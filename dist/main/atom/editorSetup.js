ca6d718d5af711772dd211b5c55ade28cf418eda
Arthur Xavier <arthur.xavierx@gmail.com>
2017-02-22 15:59:40 -0300

    Add and remove 'typescript-editor' class from <atom-text-editor> on use

--- dist/main/atom/editorSetup.js
+++ dist/main/atom/editorSetup.js
@@ -1,4 +1,9 @@
 "use strict";
 function setupEditor(editor) {
+    var editorView = atom.views.getView(editor);
+    editorView.classList.add('typescript-editor');
+    editor.onDidDestroy(function () {
+        editorView.classList.remove('typescript-editor');
+    });
 }
 exports.setupEditor = setupEditor;
