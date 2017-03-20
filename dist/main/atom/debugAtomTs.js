84664e611d300f8e707b1cf94adc32d5eb35014b
Guntars Asmanis-Graham <gunchars@gmail.com>
2017-03-19 22:05:13 -0400

    Remove some old dist files

--- dist/main/atom/debugAtomTs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var atomConfig = require("./atomConfig");
-function runDebugCode(details) {
-    if (!atomConfig.debugAtomTs)
-        return;
-}
-exports.runDebugCode = runDebugCode;
--- dist/main/lang/typescriptServices.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typescriptServices = '';
-function setTypescriptServices(path) {
-    exports.typescriptServices = path;
-}
-exports.setTypescriptServices = setTypescriptServices;
