0eb6f85a8d1e4013371cbb0cc6d5631a6b4c1573
Nikolay Yakimov <root@livid.pp.ru>
2017-12-04 01:17:08 +0300

    Add new config option to typings

--- lib/typings/atom.d.ts
+++ lib/typings/atom.d.ts
@@ -2,6 +2,7 @@ export {}
 declare module "atom" {
   interface ConfigValues {
     "atom-typescript.unusedAsInfo": boolean
+    "atom-typescript.autocompletionSuggestionPriority": number
   }
 
   interface TextBuffer {
