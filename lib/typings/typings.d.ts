dc6e26badaa5f69113af852af0eb200f80116d50
Nikolay Yakimov <root@livid.pp.ru>
2018-01-11 00:17:26 +0300

    Remove dependency on space-pen

--- lib/typings/typings.d.ts
+++ lib/typings/typings.d.ts
@@ -7,16 +7,3 @@ declare module "escape-html" {
   function escape(html: string): string
   export = escape
 }
-
-declare module "atom-space-pen-views" {
-  import * as sp from "space-pen"
-  export class SelectListView {
-    storeFocusedElement(): void
-    focusFilterEditor(): void
-    restoreFocus(): void
-    setItems(items: any[]): void
-  }
-  export class ScrollView {}
-  export class View extends sp.View {}
-  export var $: JQueryStatic
-}
