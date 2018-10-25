e01c433fce7aeaaf23b0b2dbc9bdc71a7c721b65
Nikolay Yakimov <root@livid.pp.ru>
2018-10-25 18:35:40 +0300

    Handle promises in occurence controler appropriately;clear saved markers

--- dist/main/atom/occurrence/controller.js
+++ dist/main/atom/occurrence/controller.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const atom_1 = require("atom");
 const lodash_1 = require("lodash");
-const utils_1 = require("../utils");
+const utils_1 = require("../../../utils");
+const utils_2 = require("../utils");
 class OccurenceController {
     constructor(getClient, editor) {
         this.getClient = getClient;
@@ -10,55 +11,10 @@ class OccurenceController {
         this.disposables = new atom_1.CompositeDisposable();
         this.occurrenceMarkers = [];
         this.disposed = false;
-        // tslint:disable-next-line:member-ordering
-        this.update = lodash_1.debounce(async () => {
-            if (this.disposed)
-                return;
-            if (!utils_1.isTypescriptEditorWithPath(this.editor)) {
-                this.clearMarkers();
-                return;
-            }
-            const filePath = this.editor.getPath();
-            if (filePath === undefined)
-                return;
-            const client = await this.getClient(filePath);
-            if (this.disposed)
-                return;
-            const pos = this.editor.getLastCursor().getBufferPosition();
-            try {
-                const result = await client.execute("occurrences", {
-                    file: filePath,
-                    line: pos.row + 1,
-                    offset: pos.column + 1,
-                });
-                if (this.disposed)
-                    return;
-                const ranges = result.body.map(utils_1.spanToRange);
-                const newOccurrenceMarkers = ranges.map(range => {
-                    const oldMarker = this.occurrenceMarkers.find(m => m.getBufferRange().isEqual(range));
-                    if (oldMarker)
-                        return oldMarker;
-                    else {
-                        const marker = this.editor.markBufferRange(range);
-                        this.editor.decorateMarker(marker, {
-                            type: "highlight",
-                            class: "atom-typescript-occurrence",
-                        });
-                        return marker;
-                    }
-                });
-                for (const m of this.occurrenceMarkers) {
-                    if (!newOccurrenceMarkers.includes(m))
-                        m.destroy();
-                }
-                this.occurrenceMarkers = newOccurrenceMarkers;
-            }
-            catch (e) {
-                if (window.atom_typescript_debug)
-                    console.error(e);
-            }
-        }, 100, { leading: true });
-        this.disposables.add(editor.onDidChangeCursorPosition(this.update), editor.onDidChangePath(this.update), editor.onDidChangeGrammar(this.update));
+        const debouncedUpdate = lodash_1.debounce(() => {
+            utils_1.handlePromise(this.update());
+        }, 100);
+        this.disposables.add(editor.onDidChangeCursorPosition(debouncedUpdate), editor.onDidChangePath(debouncedUpdate), editor.onDidChangeGrammar(debouncedUpdate));
     }
     dispose() {
         if (this.disposed)
@@ -70,6 +26,54 @@ class OccurenceController {
     clearMarkers() {
         for (const marker of this.occurrenceMarkers) {
             marker.destroy();
+        }
+        this.occurrenceMarkers = [];
+    }
+    async update() {
+        if (this.disposed)
+            return;
+        if (!utils_2.isTypescriptEditorWithPath(this.editor)) {
+            this.clearMarkers();
+            return;
+        }
+        const filePath = this.editor.getPath();
+        if (filePath === undefined)
+            return;
+        const client = await this.getClient(filePath);
+        if (this.disposed)
+            return;
+        const pos = this.editor.getLastCursor().getBufferPosition();
+        try {
+            const result = await client.execute("occurrences", {
+                file: filePath,
+                line: pos.row + 1,
+                offset: pos.column + 1,
+            });
+            if (this.disposed)
+                return;
+            const ranges = result.body.map(utils_2.spanToRange);
+            const newOccurrenceMarkers = ranges.map(range => {
+                const oldMarker = this.occurrenceMarkers.find(m => m.getBufferRange().isEqual(range));
+                if (oldMarker)
+                    return oldMarker;
+                else {
+                    const marker = this.editor.markBufferRange(range);
+                    this.editor.decorateMarker(marker, {
+                        type: "highlight",
+                        class: "atom-typescript-occurrence",
+                    });
+                    return marker;
+                }
+            });
+            for (const m of this.occurrenceMarkers) {
+                if (!newOccurrenceMarkers.includes(m))
+                    m.destroy();
+            }
+            this.occurrenceMarkers = newOccurrenceMarkers;
+        }
+        catch (e) {
+            if (window.atom_typescript_debug)
+                console.error(e);
         }
     }
 }
--- lib/main/atom/occurrence/controller.ts
+++ lib/main/atom/occurrence/controller.ts
@@ -1,6 +1,7 @@
 import {CompositeDisposable, DisplayMarker, TextEditor} from "atom"
 import {debounce} from "lodash"
 import {GetClientFunction} from "../../../client"
+import {handlePromise} from "../../../utils"
 import {isTypescriptEditorWithPath, spanToRange} from "../utils"
 
 export class OccurenceController {
@@ -9,10 +10,13 @@ export class OccurenceController {
   private disposed = false
 
   constructor(private getClient: GetClientFunction, private editor: TextEditor) {
+    const debouncedUpdate = debounce(() => {
+      handlePromise(this.update())
+    }, 100)
     this.disposables.add(
-      editor.onDidChangeCursorPosition(this.update),
-      editor.onDidChangePath(this.update),
-      editor.onDidChangeGrammar(this.update),
+      editor.onDidChangeCursorPosition(debouncedUpdate),
+      editor.onDidChangePath(debouncedUpdate),
+      editor.onDidChangeGrammar(debouncedUpdate),
     )
   }
 
@@ -27,54 +31,50 @@ export class OccurenceController {
     for (const marker of this.occurrenceMarkers) {
       marker.destroy()
     }
+    this.occurrenceMarkers = []
   }
 
-  // tslint:disable-next-line:member-ordering
-  private update = debounce(
-    async () => {
+  private async update() {
+    if (this.disposed) return
+    if (!isTypescriptEditorWithPath(this.editor)) {
+      this.clearMarkers()
+      return
+    }
+    const filePath = this.editor.getPath()
+    if (filePath === undefined) return
+    const client = await this.getClient(filePath)
+    if (this.disposed) return
+
+    const pos = this.editor.getLastCursor().getBufferPosition()
+
+    try {
+      const result = await client.execute("occurrences", {
+        file: filePath,
+        line: pos.row + 1,
+        offset: pos.column + 1,
+      })
       if (this.disposed) return
-      if (!isTypescriptEditorWithPath(this.editor)) {
-        this.clearMarkers()
-        return
-      }
-      const filePath = this.editor.getPath()
-      if (filePath === undefined) return
-      const client = await this.getClient(filePath)
-      if (this.disposed) return
 
-      const pos = this.editor.getLastCursor().getBufferPosition()
+      const ranges = result.body!.map(spanToRange)
 
-      try {
-        const result = await client.execute("occurrences", {
-          file: filePath,
-          line: pos.row + 1,
-          offset: pos.column + 1,
-        })
-        if (this.disposed) return
-
-        const ranges = result.body!.map(spanToRange)
-
-        const newOccurrenceMarkers = ranges.map(range => {
-          const oldMarker = this.occurrenceMarkers.find(m => m.getBufferRange().isEqual(range))
-          if (oldMarker) return oldMarker
-          else {
-            const marker = this.editor.markBufferRange(range)
-            this.editor.decorateMarker(marker, {
-              type: "highlight",
-              class: "atom-typescript-occurrence",
-            })
-            return marker
-          }
-        })
-        for (const m of this.occurrenceMarkers) {
-          if (!newOccurrenceMarkers.includes(m)) m.destroy()
+      const newOccurrenceMarkers = ranges.map(range => {
+        const oldMarker = this.occurrenceMarkers.find(m => m.getBufferRange().isEqual(range))
+        if (oldMarker) return oldMarker
+        else {
+          const marker = this.editor.markBufferRange(range)
+          this.editor.decorateMarker(marker, {
+            type: "highlight",
+            class: "atom-typescript-occurrence",
+          })
+          return marker
         }
-        this.occurrenceMarkers = newOccurrenceMarkers
-      } catch (e) {
-        if (window.atom_typescript_debug) console.error(e)
+      })
+      for (const m of this.occurrenceMarkers) {
+        if (!newOccurrenceMarkers.includes(m)) m.destroy()
       }
-    },
-    100,
-    {leading: true},
-  )
+      this.occurrenceMarkers = newOccurrenceMarkers
+    } catch (e) {
+      if (window.atom_typescript_debug) console.error(e)
+    }
+  }
 }
