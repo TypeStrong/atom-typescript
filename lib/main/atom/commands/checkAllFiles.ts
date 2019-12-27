ec521e3975b4675e7ede1d26f9efb31f395f32b2
joon-k <joon.k@kakaocorp.com>
2019-12-27 13:11:29 +0900

    Add a filter for same task blocking in checkAllFiles

--- lib/main/atom/commands/checkAllFiles.ts
+++ lib/main/atom/commands/checkAllFiles.ts
@@ -1,45 +1,79 @@
-import {addCommand} from "./registry"
+import {Disposable, File} from "atom"
+import {TypescriptServiceClient} from "../../../client/client"
+import {addCommand, Dependencies} from "./registry"
 
 addCommand("atom-text-editor", "typescript:check-all-files", deps => ({
   description: "Typecheck all files in project related to current active text editor",
   async didDispatch(editor) {
     const file = editor.getPath()
     if (file === undefined) return
-    const client = await deps.getClient(file)
 
-    const projectInfo = await client.execute("projectInfo", {
+    const client = await deps.getClient(file)
+    const result = await client.execute("projectInfo", {
       file,
       needFileNameList: true,
     })
+    const {configFileName, fileNames} = result.body!
+    const tsconfig = configFileName ? new File(configFileName).getPath() : configFileName
 
-    const files = new Set(projectInfo.body!.fileNames)
-    const max = files.size
-
-    // There's no real way to know when all of the errors have been received and not every file from
-    // the files set is going to receive a a diagnostic event (typically some d.ts files). To counter
-    // that, we cancel the listener and close the progress bar after no diagnostics have been received
-    // for some amount of time.
-    let cancelTimeout: number | undefined
-
-    const disp = client.on("syntaxDiag", evt => {
-      if (cancelTimeout !== undefined) window.clearTimeout(cancelTimeout)
-      cancelTimeout = window.setTimeout(cancel, 2000)
-
-      if ("file" in evt) files.delete(evt.file)
-      updateStatus()
-    })
-
-    deps.reportProgress({max, value: 0})
-    await client.execute("geterrForProject", {file, delay: 0})
-
-    function cancel() {
-      files.clear()
-      updateStatus()
-    }
-
-    function updateStatus() {
-      if (files.size === 0) disp.dispose()
-      deps.reportProgress({max, value: max - files.size})
-    }
+    await handleCheckAllFilesResult(file, fileNames, tsconfig, client, deps.reportProgress)
   },
 }))
+
+let buffer: {
+  max: number
+  file: string
+  files: Set<string>
+  tsconfig: string | undefined
+  disp: Disposable
+} | null = null
+
+export async function handleCheckAllFilesResult(
+  file: string,
+  fileNames: string[] | undefined,
+  tsconfig: string | undefined,
+  client: TypescriptServiceClient,
+  reportProgress: Dependencies["reportProgress"],
+): Promise<void> {
+  if (buffer && buffer.files.size !== 0) {
+    if (file === buffer.file || tsconfig === buffer.tsconfig) return
+    cancel()
+  }
+
+  const files = new Set(fileNames)
+  const max = files.size
+
+  // There's no real way to know when all of the errors have been received and not every file from
+  // the files set is going to receive a a diagnostic event (typically some d.ts files). To counter
+  // that, we cancel the listener and close the progress bar after no diagnostics have been received
+  // for some amount of time.
+  let cancelTimeout: number | undefined
+
+  const disp = client.on("syntaxDiag", evt => {
+    if (cancelTimeout !== undefined) window.clearTimeout(cancelTimeout)
+    cancelTimeout = window.setTimeout(cancel, 2000)
+
+    if ("file" in evt) files.delete(evt.file)
+    updateStatus()
+  })
+
+  buffer = {max, file, files, disp, tsconfig}
+
+  reportProgress({max, value: 0})
+  await client.execute("geterrForProject", {file, delay: 0})
+
+  function cancel() {
+    if (!buffer) return
+    buffer.files.clear()
+    updateStatus()
+  }
+
+  function updateStatus() {
+    if (!buffer) return
+    reportProgress({max: buffer.max, value: buffer.max - buffer.files.size})
+    if (buffer.files.size === 0) {
+      buffer.disp.dispose()
+      buffer = null
+    }
+  }
+}
