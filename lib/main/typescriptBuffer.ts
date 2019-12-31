d52a74db97874b324a8798e493170f48bfdb80f4
joon-k <joon.k@kakaocorp.com>
2019-12-31 12:48:40 +0900

    Fix DOM Exception in status bar when call compileOnSave

--- lib/main/typescriptBuffer.ts
+++ lib/main/typescriptBuffer.ts
@@ -55,8 +55,8 @@ export class TypescriptBuffer {
       }),
       buffer.onDidStopChanging(({changes}) => {
         if (changes.length > 0) {
-          handlePromise(this.getErrRelated(changes[0].newRange))
           this.deps.reportBuildStatus(undefined)
+          handlePromise(this.getErrRelated(changes[0].newRange))
         }
       }),
       buffer.onDidChangeText(arg => {
