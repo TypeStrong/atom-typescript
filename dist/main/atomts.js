74ced4d7881ed5a3e3655694bdbe5fe1cb101e4f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-06-08 09:51:20 -0500

    npm run build

--- dist/main/atomts.js
+++ dist/main/atomts.js
@@ -3,9 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let pluginManager;
 async function activate(state) {
     // install dependencies
-    if (!(atom.packages.isPackageLoaded("atom-ide-ui") ||
-        atom.packages.isPackageLoaded("linter") ||
-        atom.packages.isPackageLoaded("nuclide"))) {
+    const packagesProvidingUIServices = ["atom-ide-ui", "linter", "nuclide"];
+    if (!packagesProvidingUIServices.some(p => atom.packages.isPackageLoaded(p))) {
         const packageDeps = await Promise.resolve().then(() => require("atom-package-deps"));
         await packageDeps.install("atom-typescript", true);
     }
