72e8a150433a1e93a49298992831fa3766d410f6
Nikolay Yakimov <root@livid.pp.ru>
2020-10-01 18:23:39 +0300

    Remove old build artifacts

--- dist/main/atom/codefix/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CodefixProvider = void 0;
-var codefixProvider_1 = require("./codefixProvider");
-Object.defineProperty(exports, "CodefixProvider", { enumerable: true, get: function () { return codefixProvider_1.CodefixProvider; } });
-//# sourceMappingURL=index.js.map
