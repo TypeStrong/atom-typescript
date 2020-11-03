bceb838cceed13df29789d2a904350ebb24a9096
Nikolay Yakimov <root@livid.pp.ru>
2020-11-03 07:00:42 +0300

    Do not enable debug in dev mode

--- loader.js
+++ loader.js
@@ -1,6 +1,5 @@
 if (atom.inDevMode() && require.resolve("atom-ts-transpiler")) {
   console.log("Running atom-typescript in dev-mode")
-  window.atom_typescript_debug = true
   module.exports = require("./lib/main/atomts.ts")
 } else {
   module.exports = require("./dist/main.js")
