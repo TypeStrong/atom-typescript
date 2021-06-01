a09f81748fc9970c1dce323416e3ad52796eb37d
Nikolay Yakimov <root@livid.pp.ru>
2021-06-02 01:44:50 +0300

    Fix loader in dev mode

--- loader.js
+++ loader.js
@@ -1,6 +1,11 @@
-if (atom.inDevMode() && require.resolve("atom-ts-transpiler")) {
-  console.log("Running atom-typescript in dev-mode")
-  module.exports = require("./lib/main/atomts.ts")
-} else {
-  module.exports = require("./dist/main.js")
+let main = "./dist/main.js"
+try {
+  // note: require.resolve can throw for non-existent module, hence the whole try...catch dance
+  if (atom.inDevMode() && require.resolve("atom-ts-transpiler")) {
+    console.log("Running atom-typescript in dev-mode")
+    main = "./lib/main/atomts.ts"
+  }
+} catch (e) {
+  console.warn("Atom-TypeScript", e)
 }
+module.exports = require(main)
